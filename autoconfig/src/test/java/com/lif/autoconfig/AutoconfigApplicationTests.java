f639c104e033dffb37c7276f5e33d42ccb28070b
lif <798449106@qq.com>
2019-09-21 11:53:09 +0800

    提交学习spring的工程
    Signed-off-by: lif <798449106@qq.com>

--- autoconfig/src/test/java/com/lif/autoconfig/AutoconfigApplicationTests.java
+++ autoconfig/src/test/java/com/lif/autoconfig/AutoconfigApplicationTests.java
@@ -1,16 +1,36 @@
 package com.lif.autoconfig;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 测试方法
+ * @Author: lifan
+ * @Date: 2019/8/24 23:10
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AutoconfigApplicationTests {
 
+    /**
+     * 
+     * @return void
+     */
     @Test
     public void contextLoads() {
+        Assert.assertNull("");
+        System.out.println("test1");
+    }
+
+    /**
+     * 
+     * @return void
+     */
+    public void test1() {
+        System.out.println("test1");
     }
 
 }
