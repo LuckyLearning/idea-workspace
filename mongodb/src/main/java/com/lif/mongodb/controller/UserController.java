9885971a36a036e8407a6cbcc53a701f7f033461
lif <798449106@qq.com>
2019-12-01 20:42:24 +0800

    学习springboot2.x mongoDB
    Signed-off-by: lif <798449106@qq.com>

--- mongodb/src/main/java/com/lif/mongodb/controller/UserController.java
+++ mongodb/src/main/java/com/lif/mongodb/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.lif.mongodb.controller;
 
+import com.lif.mongodb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 测试页面
+     * @param
+     * @return: java.lang.String
+     * @createDate: 2019/12/1 12:50
+     */
+    @RequestMapping("/page")
+    public String page() {
+        return "page";
+    }
+
+
 
 }
---
+++ mongodb/src/main/java/com/lif/mongodb/service/UserService.java
@@ -0,0 +1,12 @@
+package com.lif.mongodb.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: lifan
+ * @Date: 2019/12/1 12:48
+ */
+@Service
+public class UserService {
+
+}
